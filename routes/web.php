4e2e497af3ca48b2d2cdd96fd214f355b985b7b3
drajin <drajin@gmail.com>
2022-01-06 02:18:56 +0100

    ready for routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+Route::resource('/posts', PostsController::class);
 
 Auth::routes();
 
